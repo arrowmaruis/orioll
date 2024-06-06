ae3a5c824e147679cb4dcf27767eef582a66e9d4
arrowmaruis <arrow2maruis@gmail.com>
2024-06-06 10:53:30 +0000

    Final 1

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -10,10 +10,20 @@ class ExampleTest extends TestCase
     /**
      * A basic test example.
      */
-    public function test_the_application_returns_a_successful_response(): void
-    {
-        $response = $this->get('/');
+    // public function test_the_application_returns_a_successful_response(): void
+    // {
+    //     $response = $this->get('/');
 
-        $response->assertStatus(200);
-    }
+    //     $response->assertStatus(200);
+    // }
+
+    public function test_the_application_returns_a_successful_response()
+{
+    $response = $this->get('/');
+
+    $response->status(); // This will output the response content to help debug
+
+    $response->assertStatus(200);
+}
+
 }
